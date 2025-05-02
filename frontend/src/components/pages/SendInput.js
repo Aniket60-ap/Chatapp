3c3686038deb476a8e9e2b24a3e7348aa120a55f
Aniket60-ap <paulaniket746@gmail.com>
2025-05-02 17:14:04 +0530

    Update SendInput.js

--- frontend/src/components/pages/SendInput.js
+++ frontend/src/components/pages/SendInput.js
@@ -12,7 +12,7 @@ function SendInput() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        `https://chatapp-backend-4xha.onrender.com/api/v1/message/send/${selectedUser?._id}`,
+        `https://chatapp-backend-bdh3.onrender.com/api/v1/message/send/${selectedUser?._id}`,
         { message },
         {
           headers: {
