482258888de5d8b36162788542e6b85558ed9762
Aniket60-ap <paulaniket746@gmail.com>
2025-05-02 16:22:23 +0530

    Update UseGetMessages.js

--- frontend/src/components/hooks/UseGetMessages.js
+++ frontend/src/components/hooks/UseGetMessages.js
@@ -11,7 +11,7 @@ function UseGetMessages() {
       try {
         axios.defaults.withCredentials = true;
         const res = await axios.get(
-          `http://localhost:4000/api/v1/message/${selectedUser._id}`
+          `https://chatapp-backend-4xha.onrender.com/api/v1/message/${selectedUser._id}`
         );
         // console.log(res);
         //store
